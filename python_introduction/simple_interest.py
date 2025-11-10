5df9b75f13de45cf057438e8e41ae2093dc11863
Tammy Aminigo <tammyaminigo65@gmail.com>
2025-11-10 11:31:09 +0100

    Add simple_interest.py file to python_introduction folder

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#Simple Interest Calculator
+
+principal = 1000 #Principal amount in dollars
+rate = 0.05 #Annual interest rate of 5% represented as a decimal
+time = 3 #Time in years
+
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
