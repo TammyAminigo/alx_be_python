ef78f8d7125879fa8fb6232861b7f4c983a78e1c
Tammy Aminigo <tammyaminigo65@gmail.com>
2025-11-10 11:09:27 +0100

    Initial commit: Add python_introduction folder containing  basic_operations.py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10 # First number
+number2 = 5 # Second number
+
+Addition = number1 + number2 # Addition operation
+Subtraction = number1 - number2 # Subtraction operation
+Multiplication = number1 * number2 # Multiplication operation
+
+print("Addition of", number1, "and", number2, "is", Addition) # Printing the result of addition
+print("Subtraction of", number1, "and", number2, "is", Subtraction) # Printing the result of subtraction
+print("Multiplication of", number1, "and", number2, "is", Multiplication) # Printing the result of multiplication
+
