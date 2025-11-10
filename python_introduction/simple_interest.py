78c350d2c1ed1a6336757e9e5ab24f962ca14e8e
Tammy Aminigo <105380252+TammyAminigo@users.noreply.github.com>
2025-11-10 11:39:17 +0100

    Fix comment formatting for interest rate

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
 #Simple Interest Calculator
 
 principal = 1000 #Principal amount in dollars
-rate = 0.05 #Annual interest rate of 5% represented as a decimal
+rate = 0.05 #Annual interest rate of 5% represented in decimals
 time = 3 #Time in years
 
 interest = principal * rate * time
